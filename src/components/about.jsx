4e6b7f02f6cda5b870b0efc7b691425946b3037c
Darshani Pathiranage <it21161742@my.sliit.lk>
2025-06-30 20:17:08 +0530

    space problem updated

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -22,16 +22,12 @@ export default function About() {
             Lanka Institute of Information Technology (SLIIT), specializing in
             Information Technology. I'm someone who’s genuinely curious about
             how things work and always eager to find better, smarter ways to
-            solve problems.
-            {/* <br></br>  */}
-            Throughout my academic journey, I’ve gained hands-on experience and
+            solve problems. Throughout my academic journey, I’ve gained hands-on experience and
             practical knowledge in areas like full-stack development,
             object-oriented programming, cloud technologies, and IT
             infrastructure. These projects have helped me build a strong
             foundation, not just technically, but also in working effectively
-            with others and adapting to challenges.
-            {/* <br></br> */}
-            Right now, I’m looking for an internship where I can put my skills
+            with others and adapting to challenges. Right now, I’m looking for an internship where I can put my skills
             into action, keep learning, and contribute to real-world projects.
             If you're looking for someone who’s motivated, collaborative, and
             ready to grow with your team, I’d love to connect.
